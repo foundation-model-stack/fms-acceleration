2a705ca4a2f7bc210ac515022f91fc42c36d1320
1000850000 user <aaron.chew1@ibm.com>
2024-07-11 09:01:28 +0000

    modified comparison script

--- scripts/benchmarks/compare_with_reference.py
+++ scripts/benchmarks/compare_with_reference.py
@@ -5,72 +5,56 @@ import matplotlib.pyplot as plt
 import os
 from numpy import linalg
 
-PLOT_COLUMNS = ["mem_torch_mem_alloc_in_bytes", "mem_peak_torch_mem_alloc_in_bytes", "train_loss", "train_tokens_per_second"]     
-INDICES = ["framework_config", "peft_method", "model_name_or_path", "num_gpus", "per_device_train_batch_size"]
-REFERENCE_FILEPATH = "scripts/benchmarks/refs/a100_80gb.csv"
+# default columns to compare
+DEFAULT_PLOT_COLUMNS = ["mem_torch_mem_alloc_in_bytes", "mem_peak_torch_mem_alloc_in_bytes", "train_loss", "train_tokens_per_second"]     
+# Used as combined identifier of experiment
+DEFAULT_INDICES = ["framework_config", "peft_method", "model_name_or_path", "num_gpus", "per_device_train_batch_size"]
+DEFAULT_OUTLIERS_DF_COLUMN_NAMES = ["scenario", *DEFAULT_INDICES, "reference", "new"]
+DEFAULT_REFERENCE_FILEPATH = "scripts/benchmarks/refs/a100_80gb.csv"
 BENCHMARK_FILENAME = "benchmarks.csv"
-FIGURE_FILENAME = "comparison.jpg"
 
 def plot_chart(ax, x, y, title, xlabel, ylabel):
     ax.scatter(x, y, s=10)
-    ax.plot()
     ax.set_title(title, fontsize=8)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     ax.axline((0, 0), slope=1)
-
-def plot_table(ax, cell_inputs, title, col_widths, col_labels):
-    table = ax.table(cellText=cell_inputs, loc="center", colWidths=col_widths, colLabels=col_labels)
-    table.scale(1, 2)
-    table.auto_set_font_size(False)
-    table.set_fontsize(8)
-    table.auto_set_column_width(0)            
-    ax.set_title(title, fontsize=10)
-    
+   
 def compare_results(df, ref, plot_columns, num_columns = 2, threshold_ratio=0.1):
     num_plots = len(plot_columns)    
-    rows = num_plots
-    fig, axs = plt.subplots(rows, 2, figsize=(20, 28))
-    fig.tight_layout(pad=5.0)
-    
+   
+    charts = []
+    total_outliers = []
     # filter ref to only those rows seen in df
     ref = ref[ref.index.isin(df.index.tolist())]
     for idx in range(num_plots):
+        _, ax = plt.subplots(figsize=(8, 8))
         column = plot_columns[idx]
-        assert (column in ref.columns) and (column in df.columns), f"Column Name `{column}` not in Dataframe"
-        ax1 = axs[idx][0]
-        ax2 = axs[idx][1]
-        ax2.axis('off')
-        
+        assert (
+            (column in ref.columns)
+            and 
+            (column in df.columns)
+        ), f"Column Name `{column}` not in Dataframe"        
+
         ref_series = ref[column].fillna(0)
         df_series = df[column].fillna(0)
-        # Calculate difference of l1 norms as a percentage on reference
-        ref_norm = linalg.norm(ref_series, ord=1)
-        df_norm = linalg.norm(df_series, ord=1)
-        norm_difference = abs(df_norm - ref_norm)/(ref_norm+1e-9)
-        # Extract outliers from reference based on % threshold on referance
+        # Extract outliers base on some threshold % difference on referance
         ds = abs(df_series-ref_series)/(ref_series+1e-9)    
-        outliers = ds.index[ds>threshold_ratio].to_list()
-        
+        outliers = ds.index[ds>threshold_ratio].to_list()        
         plot_chart(
-            ax1, 
+            ax, 
             ref_series, 
             df_series, 
             title=f"Metric: {column}", 
             xlabel="Reference", 
             ylabel="New",
-        )
-        
-        cell_inputs = [[outlier, ref_series[outlier], df_series[outlier]] for outlier in outliers] if len(outliers)>0 else [["","",""]]
-
-        plot_table(
-            ax2,
-            cell_inputs = cell_inputs,
-            title=f"Metric: {column} outliers\n\nNorm Difference={norm_difference:.3f}", 
-            col_widths=[0.9, 0.2, 0.2], 
-            col_labels=["Experiment", "Reference", "New"]
-        )                
-    return fig
+        )        
+        charts.append((ax, f"compare-{column}.jpg"))
+        total_outliers += [
+            [column, *outlier, ref_series[outlier].item(), df_series[outlier].item()]
+            for outlier in outliers
+        ]
+    return total_outliers, charts
 
 def read_df(file_path, indices, plot_columns):
     df = pd.read_csv(file_path)
@@ -78,11 +62,14 @@ def read_df(file_path, indices, plot_columns):
     df = df[plot_columns]
     return df
 
-def main(result_dir):
-    ref = read_df(REFERENCE_FILEPATH, INDICES, PLOT_COLUMNS)
-    df = read_df(os.path.join(result_dir, BENCHMARK_FILENAME), INDICES, PLOT_COLUMNS)
-    fig = compare_results(df, ref, PLOT_COLUMNS, threshold_ratio=.1)
-    plt.savefig(os.path.join(result_dir, FIGURE_FILENAME))    
+def main(result_dir, reference_benchmark_filepath, plot_columns):
+    ref = read_df(reference_benchmark_filepath, DEFAULT_INDICES, plot_columns)
+    df = read_df(os.path.join(result_dir, BENCHMARK_FILENAME), DEFAULT_INDICES, plot_columns)
+    total_outliers, charts = compare_results(df, ref, plot_columns, threshold_ratio=.1)
+    outliers_df = pd.DataFrame(total_outliers, columns=DEFAULT_OUTLIERS_DF_COLUMN_NAMES)
+    outliers_df.to_csv(os.path.join(result_dir, "outliers.csv"), index=None)
+    for chart, filename in charts:
+        chart.figure.savefig(os.path.join(result_dir, filename))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -92,10 +79,24 @@ if __name__ == "__main__":
     parser.add_argument(
         "--result_dir",
         default="benchmark_outputs",
-        help="benchmark result directory",
+        help="benchmark result directory to use for comparison",
+    )
+
+    parser.add_argument(
+        "--reference_benchmark_filepath",
+        default="scripts/benchmarks/refs/a100_80gb.csv",
+        help="file path of the csv to compare on",
+    )
+
+    parser.add_argument(
+        "--plot_columns",
+         default=DEFAULT_PLOT_COLUMNS,
+         nargs='+'
     )
 
     args = parser.parse_args()
     main(
         result_dir=args.result_dir,
+        reference_benchmark_filepath=args.reference_benchmark_filepath,
+        plot_columns=args.plot_columns,
     )
