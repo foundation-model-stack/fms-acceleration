7efbfed6f7935947f745462ccca9c98d5127a20a
1000850000 user <aaron.chew1@ibm.com>
2024-07-18 10:28:19 +0000

    remove redundant patch function

--- plugins/accelerated-peft/src/fms_acceleration_peft/autogptq_utils.py
+++ plugins/accelerated-peft/src/fms_acceleration_peft/autogptq_utils.py
@@ -64,31 +64,6 @@ def load_fsdp_gptq_patch(target_module, torch_dtype):
     )
     ModelPatcher.patch = partial(ModelPatcher.patch, torch_dtype=torch_dtype)
 
-# This function may be moved after merging
-# https://github.com/foundation-model-stack/fms-acceleration/pull/25
-def _patch_target_module(
-    to_patch: str,
-    replace_with: Any,
-    target_module: str = None,
-):
-    to_patch = to_patch.split(".")
-    assert len(to_patch) > 1, "must have an object to patch"
-
-    to_patch, obj_name_to_patch = to_patch[:-1], to_patch[-1]
-    to_patch = ".".join(to_patch)
-    source = importlib.import_module(to_patch)
-    original_obj = getattr(source, obj_name_to_patch)
-    setattr(source, obj_name_to_patch, replace_with)
-
-    if target_module is not None:
-        # reload and this should get the patched object
-        target_module = importlib.import_module(target_module)
-        importlib.reload(target_module)
-
-        # replace it
-        setattr(source, obj_name_to_patch, original_obj)
-
-
 def make_sure_no_tensor_in_meta_device(
     model,
     use_triton: bool,
