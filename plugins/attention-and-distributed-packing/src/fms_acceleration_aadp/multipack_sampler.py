b98d80ef1609e5b2650613f9ce1a873d3359fa3a
Yu Chin Fabian Lim <flim@sg.ibm.com>
2024-08-16 04:20:54 +0000

    minor update
    Signed-off-by: Yu Chin Fabian Lim <flim@sg.ibm.com>

--- plugins/attention-and-distributed-packing/src/fms_acceleration_aadp/multipack_sampler.py
+++ plugins/attention-and-distributed-packing/src/fms_acceleration_aadp/multipack_sampler.py
@@ -20,7 +20,8 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-taken from https://github.com/imoneoi/multipack_sampler
+taken from https://github.com/imoneoi/multipack_sampler with some modifications 
+taken from https://github.com/instructlab/training/blob/main/src/instructlab/training/multipack_sampler.py
 """
 
 # Standard
