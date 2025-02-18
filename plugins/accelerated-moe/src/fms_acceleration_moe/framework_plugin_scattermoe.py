b2cec7e8852298956fd6906b27fe00af8d783de0
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-02-18 14:29:14 +0530

    feat: extend support to GraniteMoeSharedForCausalLM
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- plugins/accelerated-moe/src/fms_acceleration_moe/framework_plugin_scattermoe.py
+++ plugins/accelerated-moe/src/fms_acceleration_moe/framework_plugin_scattermoe.py
@@ -40,7 +40,7 @@ class ScatterMoEAccelerationPlugin(AccelerationPlugin):
     # if we decide to extract the kernels, then we do not need to anymore,
     # https://github.com/foundation-model-stack/fms-acceleration/issues/105
 
-    restricted_model_archs = ["GraniteMoeForCausalLM", "MixtralForCausalLM"]
+    restricted_model_archs = ["GraniteMoeForCausalLM", "MixtralForCausalLM", "GraniteMoeSharedForCausalLM"]
 
     def __init__(self, configurations: Dict[str, Dict]):
         super().__init__(configurations)
