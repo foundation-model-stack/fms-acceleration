893f4cd6229d8df05fdb8d97aba2464e9bd9fd55
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-11-14 18:05:27 +0530

    feat: add support for mamba cp
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- plugins/mamba-cp/src/fms_acceleration_mcp/utils/utils.py
+++ plugins/mamba-cp/src/fms_acceleration_mcp/utils/utils.py
@@ -80,9 +80,11 @@ def patch_mamba_layers_with_cp_head(
             if hasattr(layer, "mamba") and layer.mamba is not None:
                 print("mamba layer found")
                 mamba_layer = Mamba2CP(**config_ssm, **cp_args)
+                dtype = layer.mamba.dtype
+                device = layer.mamba.device
                 mamba_layer.load_state_dict(layer.mamba.state_dict())
                 setattr(layer, "mamba", mamba_layer)
-                layer.to(device)
+                layer.to(dtype).to(device)
 
     if hasattr(model, "tie_weights"):
         model.tie_weights()
